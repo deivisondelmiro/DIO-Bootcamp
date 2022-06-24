cc36facace62471b0feb6e4df7d3a003add184af
Deivison <deivisondelmirocor100@gmail.com>
2022-06-23 22:35:58 -0300

    Novos assuntos

---
+++ Módulos/Módulo II/09 - Map, Filter e Reduce/filter.js
@@ -0,0 +1,5 @@
+const frutas = ['maçã fuji', 'maçã verde', 'laranja', 'abacaxi'];
+
+frutas.filter((frutas) => frutas.includes('maçã'));
+
+console.log(frutas.filter((frutas) => frutas.includes('maçã')));
---
+++ Módulos/Módulo II/09 - Map, Filter e Reduce/map.js
@@ -0,0 +1,13 @@
+// map
+/* const array = [1, 2, 3, 4, 5];
+
+array.map((item) => item * 2);
+
+console.log(array.map((item) => item * 2)); */
+
+// forEach
+/* const array = [1, 2, 3, 4, 5];
+
+array.forEach((item) => item * 2);
+
+console.log(array.forEach((item) => item * 2)); */
