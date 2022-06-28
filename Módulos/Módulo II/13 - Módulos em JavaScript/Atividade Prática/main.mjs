912f3307e56556d9909068a9dd4e91212733eec5
Deivison <deivisondelmirocor100@gmail.com>
2022-06-27 23:25:09 -0300

    Novos arquivos

---
+++ Módulos/Módulo II/13 - Módulos em JavaScript/Atividade Prática/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./functions.mjs";
+
+console.log(mostraIdade("Deivison", 22));
+console.log(mostraCidade("Deivison", "Campo Alegre"));
+console.log(mostraHobby("Deivison", "Jogar"));
